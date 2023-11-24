902c59610f2e831b4ba2530b230ceba69bff95f0
VicVapor <victorj.gutierrezp@konradlorenz.edu.co>
2023-11-23 23:19:02 -0500

    test(Test): Test
    Text of branch 2

--- src/main/java/com/mycompany/unittest/Operations.java
+++ src/main/java/com/mycompany/unittest/Operations.java
@@ -4,14 +4,7 @@
  */
 package com.mycompany.unittest;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
-
 /**
- *
  * @author vicjo
  */
 public class Operations {
@@ -24,9 +17,7 @@ public class Operations {
 
     public static int randomNumer(int n) {
 
-        int n30 = (int) (n + (n * 0.3));
-
-        return n30;
+        return (int) (n + (n * 0.3));
     }
 
     public static int sumOfDigits(String digits) {
@@ -35,12 +26,11 @@ public class Operations {
         char[] aux = digits.toCharArray();
 
         System.out.println("Cadena: " + digits);
-        
-        
-        
+
+
         for (char i : aux) {
-            String str = new Character((char) i).toString();
-            suma+=Integer.parseInt(str);
+            String str = Character.toString(i);
+            suma += Integer.parseInt(str);
         }
 
         // System.out.println("Suma total: "+suma);
